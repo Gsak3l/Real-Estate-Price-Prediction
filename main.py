12e363d922f288d4a5eeb4253bc834c7a2816d16
George Sakellariou <gsakel5@gmail.com>
2021-05-16 20:36:14 +0300

    found new dataset

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ import matplotlib
 matplotlib.rcParams['figure.figsize'] = (20, 10)
 
 # getting to know the dataframe a bit more
-df1 = pd.read_csv('data/Houses.csv')
+df1 = pd.read_csv('./data/Bengaluru_House_Data.csv')
 print(df1.head())
 print(df1.shape)
 # df1 = df1.groupby('city').nunique()
 # print(df1)
 # print(df1.shape)
 
-print(df1.groupby('city')['city'].agg('count'))
+print(df1.groupby('area_type')['area_type'].agg('count'))
 
 # dropping some columns that are not that important
-df2 = df1.drop(['id', 'latitude', 'longitude'], axis='columns')
+df2 = df1.drop(['area_type', 'availability'], axis='columns')
 print(df2.head())
 
 print(df2.isnull().sum())
