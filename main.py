0cde9830824ca42ba31026562f4598575ee303d8
George Sakellariou <gsakel5@gmail.com>
2021-05-16 20:58:33 +0300

    dropped less important fields, created new bedroom field that replaced the size field

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ matplotlib.rcParams['figure.figsize'] = (20, 10)
 
 # getting to know the dataframe a bit more
 df1 = pd.read_csv('./data/Bengaluru_House_Data.csv')
-print(df1.head())
-print(df1.shape)
-# df1 = df1.groupby('city').nunique()
-# print(df1)
-# print(df1.shape)
 
-print(df1.groupby('area_type')['area_type'].agg('count'))
+# print(df1.groupby('area_type')['area_type'].agg('count'))
 
 # dropping some columns that are not that important
-df2 = df1.drop(['area_type', 'availability'], axis='columns')
-print(df2.head())
+df2 = df1.drop(['area_type', 'availability', 'society'], axis='columns')
 
-print(df2.isnull().sum())
+# checking how many null values there are
+# print(df2.isnull().sum())
 
+# dropping null and nan values and checking what changes occurred
 df3 = df2.dropna()
 print(df3.isnull().sum())
+# print(df2.shape, df3.shape)
 
-print(df3['size'])
+print(df3['size'].unique())
+
+# bhk = rooms i guess
+df3['bedrooms'] = df3['size'].apply(lambda x: int(x.split(' ')[0]))
+df4 = df3.drop('size', axis='columns')
+print(df4.head())
+print(df4['bedrooms'].unique())
