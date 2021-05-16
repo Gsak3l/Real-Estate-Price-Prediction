0e86b0ffcf512340724dd98af07f8a06a7f10632
George Sakellariou <gsakel5@gmail.com>
2021-05-16 18:58:36 +0300

    dataset makes this way too easy, csv file has to be changed

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from flask import Flask
+import numpy as np
+import matplotlib
+
+matplotlib.rcParams['figure.figsize'] = (20, 10)
+
+# getting to know the dataframe a bit more
+df1 = pd.read_csv('data/Houses.csv')
+print(df1.head())
+print(df1.shape)
+# df1 = df1.groupby('city').nunique()
+# print(df1)
+# print(df1.shape)
+
+print(df1.groupby('city')['city'].agg('count'))
+
+# dropping some columns that are not that important
+df2 = df1.drop(['id', 'latitude', 'longitude'], axis='columns')
+print(df2.head())
+
+print(df2.isnull().sum())
+
+df3 = df2.dropna()
+print(df3.isnull().sum())
+
+print(df3['size'])
